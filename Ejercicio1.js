a4d892f5d28f043783873615f0944a6c9d5a0466
Betza <betza.otaku.karla@gmail.com>
2025-04-11 17:03:08 -0600

    Ejercicio1-Betza

---
+++ Ejercicio1.js
@@ -0,0 +1,8 @@
+/*1. Edad del usuario
+Tema: declaración de variable
+Declara una variable llamada edad y asígnale el valor 21.
+Muestra un mensaje en consola que diga
+"Tu edad es 21 años".*/
+
+let edad = 21;
+console.log("Tu edad es " + edad + " años.");
