7828d693e5d7599195cf2248df3ec49d18405f32
Betza <betza.otaku.karla@gmail.com>
2025-04-11 17:22:13 -0600

    Ejercicio9- Operadores de comparacion- Bet

---
+++ Ejercicio9.js
@@ -0,0 +1,17 @@
+/*9. Comparación entre dos números
+
+Tema: operadores de comparación
+
+let numeroA = 15
+let numeroB = 20
+Muestra si numeroA es mayor, menor o igual que numeroB.*/
+
+let numeroA = 15
+let numeroB = 20
+
+if (numeroA > numeroB) {    
+    console.log("numeroA es mayor que numeroB")
+}
+else if (numeroA < numeroB) {
+    console.log("numeroA es menor que numeroB")
+}
