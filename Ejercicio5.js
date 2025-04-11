9db4c92f45c62d65750666657d22dc0081e50a44
Betza <betza.otaku.karla@gmail.com>
2025-04-11 17:14:44 -0600

    Ejercicio5- Valores especiales -Bet

---
+++ Ejercicio5.js
@@ -0,0 +1,12 @@
+/*5. Tipos especiales: null y undefined
+
+Tema: valores especiales
+
+let valorNulo = null
+let valorIndefinido
+Muestra el tipo de cada uno usando typeof*/
+
+let valorNulo = null
+let valorIndefinido
+console.log(typeof valorNulo) // object
+console.log(typeof valorIndefinido) // undefined
