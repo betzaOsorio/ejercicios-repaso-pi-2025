a94450a8632eea5bfbbb9d1616c63e86dc447328
Betza <betza.otaku.karla@gmail.com>
2025-04-11 17:11:39 -0600

    Ejercicio 4-Sumar texto numerico convertido-Bet

---
+++ Ejercicio4.js
@@ -0,0 +1,15 @@
+/*Sumar texto numérico convertido
+
+Tema: conversión de string a number
+
+let edadTexto = '25'
+Convierte la variable a número y suma 5.
+
+Imprime el resultado final:
+
+"Edad más 5: 30"*/
+
+let edadTexto = '25'
+let edadNumero = Number(edadTexto) 
+let resultado = edadNumero + 5
+console.log("Edad más 5: " + resultado)
