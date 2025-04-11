2f26c751c259bece25bfe35e1aab1ed34782cb4d
Betza <betza.otaku.karla@gmail.com>
2025-04-11 17:08:39 -0600

    Ejercicio3- Verificar tipo de dato booleano-Bet

---
+++ Ejercicio3.js
@@ -0,0 +1,13 @@
+/*Verificar tipo de dato booleano
+
+Tema: typeof
+
+let estaRegistrado = true
+Imprime en consola el mensaje:
+
+"El tipo de dato de estaRegistrado es boolean"*/
+
+let estaRegistrado = true;
+if (typeof estaRegistrado === 'boolean') {
+  console.log("El tipo de dato de estaRegistrado es boolean");
+}
