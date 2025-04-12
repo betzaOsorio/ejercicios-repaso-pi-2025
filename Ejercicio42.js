afcc5fc339d975262251b4216bb554d039559352
Betza <betza.otaku.karla@gmail.com>
2025-04-11 19:52:57 -0600

    Ejercicio42

---
+++ Ejercicio42.js
@@ -0,0 +1,12 @@
+/*42. Agregar un producto al carrito
+
+Tema: push()
+
+const carrito = ['Pan', 'Leche', 'Huevos']
+Agrega "Queso" al final del arreglo.
+
+Muestra el carrito actualizado.*/
+
+const carrito = ['Pan', 'Leche', 'Huevos'];
+carrito.push('Queso'); // Agrega "Queso" al final del arreglo
+console.log(carrito); // Muestra el carrito actualizado
