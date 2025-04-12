772744c79bb67c9f9fc2447b3b50892d84eae10f
Betza <betza.otaku.karla@gmail.com>
2025-04-11 19:22:49 -0600

    Ejercicio24

---
+++ Ejercicio24.js
@@ -0,0 +1,13 @@
+/*24. Contador del 1 al 10
+
+Tema: while
+
+Imprime los números del 1 al 10 usando un ciclo while.
+
+Cada número debe aparecer en una línea.*/
+
+let contador = 1; // Inicializa el contador en 1
+while (contador <= 10) { // Mientras el contador sea menor o igual a 10
+    console.log(contador); // Imprime el contador
+    contador++; // Incrementa el contador en 1
+}
