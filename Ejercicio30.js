4d38a3ca46480055f24850aa617c48c51f610f0e
Betza <betza.otaku.karla@gmail.com>
2025-04-11 19:33:25 -0600

    Ejercicio30

---
+++ Ejercicio30.js
@@ -0,0 +1,13 @@
+/*30. Sumar números del arreglo
+
+Tema: acumulador
+
+const numeros = [5, 10, 15, 20]
+Suma todos los elementos del arreglo usando for.*/
+
+const numeros = [5, 10, 15, 20];
+let suma = 0;
+for (let i = 0; i < numeros.length; i++) {
+    suma += numeros[i];
+}
+console.log('La suma de los números es:', suma);
